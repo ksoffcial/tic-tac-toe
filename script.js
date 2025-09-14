1a17d59284be89bf52944ac3254eb353c519d003
kishan dev sah <sonofsahuji@gmail.com>
2025-09-14 15:16:41 +0530

    Tic Tac Toe

---
+++ script.js
@@ -0,0 +1,81 @@
+const boxes = document.querySelectorAll(".box")
+const resetBtn = document.querySelector("#resetBtn")
+const newGame = document.querySelector("#newGame")
+const winText = document.querySelector("#winner")
+const mainCont = document.querySelector("#main")
+const winCont = document.querySelector("#winCont")
+
+let paler_x = true;
+
+let winNmbr = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
+
+
+let palyCount = 0;
+boxes.forEach((box, idx) => {
+    box.addEventListener('click', () => {
+        if (paler_x) {
+            box.innerText = "X";
+            box.style.color = "red"
+            paler_x = false;
+        } else {
+            box.innerText = "0";
+            box.style.color = "blue"
+            paler_x = true;
+        }
+        palyCount++;
+        console.log(palyCount)
+        let getwinner = getWinner();
+
+        if (palyCount === 9 && !getwinner) {
+            drawGame();
+        }
+    })
+
+})
+
+newGame.addEventListener("click", () => {
+    mainCont.style.display = "flex"
+    winCont.style.display = "none"
+    for (let box of boxes) {
+        box.innerText = ""
+    }
+})
+
+const drawGame = () => {
+    winText.innerText = `Game has draw 😷`;
+    mainCont.style.display = "none"
+    winCont.style.display = "flex"
+    palyCount = 0;
+}
+
+
+const showWinnner = (winner) => {
+    winText.innerText = `Congutalation ${winner} is a winner!🎉`;
+    mainCont.style.display = "none"
+    winCont.style.display = "flex"
+    palyCount = 0;
+}
+
+const getWinner = () => {
+    for (let nmbr of winNmbr) {
+        let play1 = boxes[nmbr[0]].innerText;
+        let play2 = boxes[nmbr[1]].innerText;
+        let play3 = boxes[nmbr[2]].innerText;
+
+        if (play1 != "" && play2 != "" && play3 != "") {
+            if (play1 === play2 && play2 === play3) {
+                showWinnner(play1)
+                return true;
+            }
+        }
+    }
+}
+
+resetBtn.addEventListener("click", () => {
+    for (let box of boxes) {
+        box.innerText = ""
+    }
+    palyCount = 0;
+})
+
+
